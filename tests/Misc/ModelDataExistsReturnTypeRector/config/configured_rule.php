fe487e261753cf505aadab5af0e60e9d8138b748
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-04-08 07:44:26 +1200

    ENH: fixing exists method to have :bool for classes extendeing ModelData

--- tests/Misc/ModelDataExistsReturnTypeRector/config/configured_rule.php
+++ tests/Misc/ModelDataExistsReturnTypeRector/config/configured_rule.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Utils\Rector\ModelDataExistsReturnTypeRector;
+
 
 return RectorConfig::configure()
     ->withRules([
-        ModelDataExistsReturnTypeRector::class,
+        \Netwerkstatt\SilverstripeRector\Rector\Misc\ModelDataExistsReturnTypeRector::class,
     ]);
