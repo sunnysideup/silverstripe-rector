8d363d98b120224ed44b0d835979dbcdf171a61f
NT@NobleStorm <noblestorm@sunnysideup.co.nz>
2026-04-13 15:29:27 +1200

    ENH: add rector rule RenameCurrentPageIdToCurrentRecordIdRector

---
+++ src/Rector/CMS/RenameCurrentPageIdToCurrentRecordIdRector.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netwerkstatt\SilverstripeRector\Rector\CMS;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
+use PHPStan\Type\ObjectType;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class RenameCurrentPageIdToCurrentRecordIdRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Rename CMSPageEditController::currentPageID() to currentRecordID()',
+            [
+                new CodeSample(
+                    '$controller->currentPageID();',
+                    '$controller->currentRecordID();'
+                )
+            ]
+        );
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isName($node->name, 'currentPageID')) {
+            return null;
+        }
+
+        // Use ObjectType without leading backslash to leverage PHPStan's recursive reflection
+        if (! $this->isObjectType($node->var, new ObjectType('SilverStripe\CMS\Controllers\CMSPageEditController'))) {
+            return null;
+        }
+
+        // Mutate the AST node directly
+        $node->name = new Identifier('currentRecordID');
+
+        return $node;
+    }
+}
