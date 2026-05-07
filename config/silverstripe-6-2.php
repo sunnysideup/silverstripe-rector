71af9b464fc4f0a669c0a065544bf6c1eb464987
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-05-07 17:45:57 +1200

    FIX: cleanup

--- config/silverstripe-6-2.php
+++ config/silverstripe-6-2.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Netwerkstatt\SilverstripeRector\Rector\ORM\GetIDListToColumnIDRector;
 use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\MethodCall\SpatieEnumMethodCallToEnumConstRector;
 use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
 
@@ -12,4 +13,11 @@ return static function (RectorConfig $rectorConfig): void {
         new MethodCallRename('SilverStripe\Forms\FieldList', 'dataFields', 'getDataFields'),
     ]);
     $rectorConfig->rule(GetIDListToColumnIDRector::class);
+    // This rector causes issues with the SpatieEnumMethodCallToEnumConstRector, so we skip it for now until we can find a solution.
+    // HERE IS THE ERROR WE GET:
+    // -        $records = $ownerClass::get()->where(implode(' OR ', $where));
+    // +        $records = ownerClass::GET->where(implode(' OR ', $where));
+    $rectorConfig->skip([
+        SpatieEnumMethodCallToEnumConstRector::class,
+    ]);
 };
